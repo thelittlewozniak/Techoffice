e7cf6c8d51e41c520553daa150f1ec78f7062d78
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-04-21 06:07:05 +0200

    Little Sitter Web

---
+++ Little Sister Web/Little Sister Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Little_Sister_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.title = "Little Siter";
+            ViewBag.titre = "Little Sister";
+            return View();
+        }
+        public ActionResult Accueil()
+        {
+            ViewBag.title = "Accueil";
+            ViewBag.titre = "Accueil";
+            return View();
+        }
+    }
+}
---
+++ Little Sister Web/Little Sister Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Little_Sister_Web.MvcApplication" Language="C#" %>
