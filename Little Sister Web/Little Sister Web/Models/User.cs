509372b4dd07dba0f474e74faab17c6c2cd2b854
Unknown <simon.gauthier@condorcet.be>
2018-04-21 14:38:19 +0200

    trololol
    trililili

---
+++ Little Sister Web/Little Sister Web/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Little_Sister_Web.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int LastPosition { get; set; }
+        public string AppToken { get; set; }
+        public DateTime LastPositionTime { get; set; }
+        public bool Ghostmode { get; set; }
+        public string Name { get; set; }
+        public User(string i, string m, int l, string a, DateTime t, bool g, string n)
+        {
+            Id = i;
+            Email = m;
+            LastPosition = l;
+            AppToken = a;
+            LastPositionTime = t;
+            Ghostmode = g;
+            Name = n;
+        }
+    }
+}
