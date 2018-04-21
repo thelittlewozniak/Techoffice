fd540ab761f9c47d4d1d34af0e4156dd639bc14d
Unknown <vercruysse.xavier@gmail.com>
2018-04-21 02:21:48 +0200

    Working on notifications

---
+++ android/LittleSister/app/src/main/java/lessalopards/littlesister/InstanceIdService.java
@@ -0,0 +1,58 @@
+package lessalopards.littlesister;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by xvercruysse on 21/04/2018.
+ */
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+    public InstanceIdService() {
+        super();
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        //sends this token to the server
+        sendToServer(token);
+    }
+
+    private void sendToServer(String token) {
+
+        try {
+            URL url = new URL("https://www.whatsthatlambda.com/store");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+
+            connection.setRequestMethod("POST");
+
+            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+
+            dos.writeBytes("token=" + token);
+
+            connection.connect();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                // Do whatever you want after the
+                // token is successfully stored on the server
+            }
+            dos.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- android/LittleSister/app/src/main/java/lessalopards/littlesister/MainActivity.java
+++ android/LittleSister/app/src/main/java/lessalopards/littlesister/MainActivity.java
@@ -3,11 +3,13 @@ package lessalopards.littlesister;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,19 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Choose authentication providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()
-        );
-
         // Create and launch sign-in intent
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build()))
                         .build(),
                 RC_SIGN_IN);
-
     }
 
     @Override
@@ -46,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 //Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                //todo send user id to azure DB
+                Log.d("TOKEN", FirebaseInstanceId.getInstance().getToken());
 
             } else {
                 //Sign in failed, check response for error code
