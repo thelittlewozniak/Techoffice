77e0b0fae0b6dac677f4992b5bc990624316c55a
thelittlewozniak <nathan.pire@gmail.com>
2018-04-21 02:22:10 +0200

    new

--- LittleSister/LittleSister/Program.cs
+++ LittleSister/LittleSister/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
 
 namespace LittleSister
 {
@@ -10,6 +13,22 @@ namespace LittleSister
     {
         static void Main(string[] args)
         {
+            VideoCapture videoCapture = new VideoCapture(0);
+            videoCapture.Start();
+            Console.WriteLine(videoCapture.CaptureSource);
+            while (true)
+            {
+                videoCapture.ImageGrabbed += VideoCapture_ImageGrabbed;
+            }
+        }
+
+        private static void VideoCapture_ImageGrabbed(object sender, EventArgs e)
+        {
+            Console.WriteLine("reussi");
+            VideoCapture capture = (VideoCapture)sender;
+            //capture.Grab();
+            Mat image=new Mat();
+            capture.Retrieve(image,0);
         }
     }
 }
